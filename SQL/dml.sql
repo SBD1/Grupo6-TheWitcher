3b5a9f819865d25422ad10be84b62e57348cb275
higton <matpaulo@hotmail.com.br>
2022-03-27 12:07:13 -0300

    create dml file

---
+++ SQL/dml.sql
@@ -0,0 +1,13 @@
+INSERT INTO Habilidade (nome, tipo) VALUES 
+('Fast Attack', 'Combat'), 
+('Aard Intensity', 'Signs'), 
+('Refreshment', 'Alchemy'), 
+('Heightened Tolerance', 'Alchemy'), 
+('Survival Instinct', 'Combat'); 
+
+INSERT INTO Personagem (nome, gold, vida, ataque, defesa) VALUES 
+('Geralt of Rivia', 0, 100, 20, 5),
+('Lambert', 2000, 1000, 50, 20),
+('Triss Merigold', 500, 100, 50, 5),
+('Eskel', 3000, 100, 50, 20),
+('Vesemir', 100, 2000, 200, 30);
