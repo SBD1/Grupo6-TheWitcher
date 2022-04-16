cc59633a3a096c296dbbbde71fab23b32e1559bb
higton <matpaulo@hotmail.com.br>
2022-04-16 18:21:57 -0300

    update sql tables

--- SQL/criarTabelas.sql
+++ SQL/criarTabelas.sql
@@ -71,12 +71,18 @@ CREATE TABLE itens_equipados (
 );
 
 CREATE TABLE mochila (
+	id int4 NOT NULL GENERATED ALWAYS AS IDENTITY,
 	id_personagem int4 NOT NULL,
-	peso float8 NULL,
 	capacidade int4 NULL,
+	CONSTRAINT mochila_pk PRIMARY KEY (id),
+	CONSTRAINT personagem_fk FOREIGN KEY (id_personagem) REFERENCES personagem(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE mochila_guarda (
+	mochila int4 NULL,
 	item int4 NULL,
 	CONSTRAINT instancia_item_fk FOREIGN KEY (item) REFERENCES instancia_item(id),
-	CONSTRAINT personagem_fk FOREIGN KEY (id_personagem) REFERENCES personagem(id) ON DELETE CASCADE ON UPDATE CASCADE
+	CONSTRAINT mochila_fk FOREIGN KEY (mochila) REFERENCES mochila(id)
 );
 
 CREATE TABLE instancia_monstro (
